f17f9b42aefdbe9fcddc12ceb09b00b3e4e8bec5
FelihleNcongwane <ncongwanefelihle02@gmail.com>
2023-03-30 16:24:47 -0700

    Magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -4,18 +4,21 @@ int main(void)
 
 {
 	int n;
+	int a[5];
 	int *p;
 
-	n = 98;
+	a[2] = 1024;
 	p = &n;
-
-	printf("value of n is: %d\n", n);
-	printf("Address of 'n' is: %p\n", &n);
-	printf("value of 'p' is: %p\n", p);
-	*p = 400;
-	printf("value of n is: %d\n", n);
-	printf("Address of pointer p is: %p\n", &p);
-
+	/*
+	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
+	 */
+	*(p + 5) = 98;
+	/* ...so that this rints 98\n*/
+	printf("a[2] = %d\n", a[2];
 	return (0);
-
 }
