9ade98340a76c613a687ad4c1efc0eac893ca305
FelihleNcongwane <ncongwanefelihle02@gmail.com>
2023-04-03 19:47:59 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - Point of Entry
+ * @s: Input
+ * @c: Input
+ * Return: 0 Always on Success
+ */
+
+char *_strchr(char *s, char c)
+
+{
+	int i = 0;
+
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+
+	return (0);
+}
