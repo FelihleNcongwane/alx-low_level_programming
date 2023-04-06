232fe216dd23bf4d17c71842ae633df06be5451c
FelihleNcongwane <ncongwanefelihle02@gmail.com>
2023-04-06 15:02:57 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - a function like puts();
+ * @s: Input
+ * Return: 0 Always on Success
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
+}
