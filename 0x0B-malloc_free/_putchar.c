5ac4c0dd89c40e640a48dfb1c2559f6d03d07cf4
FelihleNcongwane <ncongwanefelihle02@gmail.com>
2023-04-11 11:37:57 -0700

    The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes character c to stdout
+ * @c: The character to print
+ *
+ * Return: 1 if successful.
+ * On error, -1 is returned, and errno is appropriately set.
+ */
+
+int _putchar(char c)
+
+{
+	return (write(1, &c, 1));
+}
