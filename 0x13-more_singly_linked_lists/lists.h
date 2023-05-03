b7efaf152b95b871f960347ad55480baeaed8fb6
Felihle Ncongwane <ncongwanefelihle02@gmail.com>
2023-05-02 18:59:03 -0700

    HeaderFile

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -9,16 +9,15 @@
  * @n: integer
  * @next: points to the next node
  *
- * Description: singly linked list node structure
- *
+ * Description: singly linked list node structure for Holberton project
  */
-
-typedef struct listint_s {
+typedef struct listint_s
+{
     int n;
     struct listint_s *next;
 } listint_t;
 
-/* Function prototypes */
+/* Function Prototypes */
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -35,4 +34,4 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-#endif
+#endif /* LISTS_H */
