c0d979e394b484bc444319cd3aed73dc252ed7f8
Felihle Ncongwane <ncongwanefelihle02@gmail.com>
2023-05-04 21:20:09 -0700

    0

--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -5,14 +5,9 @@
  * _putchar - writes a character to stdout
  * @c: the character to write
  *
- * Return: 1 on success, or -1 on failure
+ * Return: On 1 on success, or -1 on failure
  */
 int _putchar(char c)
 {
-	/* Write the character to stdout */
-	if (write(1, &c, 1) == -1)
-		return (-1);
-
-	/* Return success */
-	return (1);
+	return (write(1, &c, 1));
 }
