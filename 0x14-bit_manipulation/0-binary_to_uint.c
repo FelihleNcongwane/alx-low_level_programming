0cb927d365ca72a6557fc42ca8ac70309e7e8a47
Felihle Ncongwane <ncongwanefelihle02@gmail.com>
2023-05-04 21:28:19 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,48 +1,33 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: pointer to the binary number as a string
+ * binary_to_uint - converts a binary number to an unsigned integer
+ * @binary: pointer to the binary number as a string
  *
- * Return: the unsigned int value of the binary number, or 0 if b is NULL or
- * contains a character that is not '0' or '1'
+ * Return: the unsigned integer value of the binary number, or 0 if binary is
+ * NULL or contains a character that is not '0' or '1'
  */
-unsigned int binary_to_uint(const char *b)
+unsigned int binary_to_uint(const char *binary)
 {
 	/* Initialize variables */
+	int index;
 	unsigned int decimal_value = 0;
-	int i = 0;
 
 	/* Check for NULL input */
-	switch (b == NULL)
-	{
-		case true:
-			return (0);
-		default:
-			break;
-	}
+	if (binary == NULL)
+		return (0);
 
 	/* Traverse the binary string and convert to decimal */
-	while (b[i] != '\0')
+	for (index = 0; binary[index] != '\0'; index++)
 	{
 		/* Check for invalid characters */
-		switch (b[i])
-		{
-			case '0':
-				/* Do nothing */
-				break;
-			case '1':
-				/* Convert binary digit to decimal */
-				decimal_value = (decimal_value << 1) | 1;
-				break;
-			default:
-				return (0);
-		}
+		if (binary[index] != '0' && binary[index] != '1')
+			return (0);
 
-		/* Move to next character in string */
-		i++;
+		/* Convert binary digit to decimal */
+		decimal_value = (decimal_value << 1) | (binary[index] - '0');
 	}
 
 	/* Return the decimal value of the binary string */
 	return (decimal_value);
-}
+}i
